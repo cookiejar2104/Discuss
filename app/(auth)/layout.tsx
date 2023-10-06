7e06a124d203438a0473e140f1e69909f875e018
cookie_jar <cptpricecod098@gmail.com>
2023-10-06 14:23:50 +0530

    first

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+export const metadata = {
+    title: 'webapp',
+    description: 'A Nextjs discussion platform'
+}
+const inter = Inter({subsets: ["latin"]}) 
+
+
+export default function RootLayout({children}:{children: React.ReactNode}){
+    return(
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-[rgb(101, 96, 96)]`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
